1eafb05906025d4803baa7fe6ee0a779179e60f8
Guy Langston <guylangston@gmail.com>
2022-01-17 12:46:06 +0000

    WIP

---
+++ src/kv.core/KeyValue.cs
@@ -0,0 +1,31 @@
+namespace kv.core;
+
+public record KeyValue
+{
+    public KeyValue(string key, string value, string? typeHint, DateTime lastAccess, string lastAccessUser, DateTime created, string createdUser, DateTime modified, string modifiedUser)
+    {
+        Key = key;
+        Value = value;
+        TypeHint = typeHint;
+        LastAccess = lastAccess;
+        LastAccessUser = lastAccessUser;
+        Created = created;
+        CreatedUser = createdUser;
+        Modified = modified;
+        ModifiedUser = modifiedUser;
+    }
+
+    public string Key { get; init; }
+    public string Value { get; init; }
+    public string? TypeHint { get; init; }
+
+    public DateTime LastAccess { get; init; }
+    public string LastAccessUser { get; init; }
+
+    public DateTime Created { get; init; }
+    public string CreatedUser { get; init; }
+
+    public DateTime Modified { get; init; }
+    public string ModifiedUser { get; init; }
+}
+
---
+++ src/kv.core/interfaces.cs
@@ -0,0 +1,15 @@
+﻿namespace kv.core;
+
+public interface IKeyValueProvider 
+{
+   IReadOnlyCollection<KeyValue> GetAll();
+   bool TryGet(string key, out KeyValue res);
+   void Store(KeyValue up);
+   void Delete(string key);
+}
+
+public class ProviderConfig
+{
+   public string Provider { get; set; }
+   public IReadOnlyDictionary<string, string> Args { get; set; }
+}
